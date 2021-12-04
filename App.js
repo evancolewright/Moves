767b993366292464543c5bb1926788924a893966
Evan Wright <ewright@valorsystems.com>
2021-12-04 00:54:13 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import tailwind from 'tailwind-rn';
+import React from 'react'
+import { SafeAreaView, Text, View } from 'react-native'
+import tailwind from 'tailwind-rn'
 
 export default function App() {
+  console.log('Hello!')
+
   return (
     <SafeAreaView style={tailwind('flex-1 items-center justify-center')}>
       <View style={tailwind('bg-blue-500 px-5 py-3 rounded-full')}>
-        <Text style={tailwind('text-white font-semibold text-lg')}>
-          Hello Tailwind 👋
-        </Text>
+        <Text style={tailwind('text-white font-semibold text-lg')}>Hello Tailwind 👋</Text>
       </View>
     </SafeAreaView>
-  );
+  )
 }
+;``
